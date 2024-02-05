9f59cc999feff4a4d99a24a18da60a94d0146310
pyshechka <141571548+miniguts@users.noreply.github.com>
2024-02-05 20:34:18 +0600

    aa

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name =  models.CharField(max_length = 225, verbose_name = 'Имя')
+    email = models.EmailField(verbose_name = 'Почта')
+    age = models.IntegerField(verbose_name = 'Возраст')
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+    
+    def str(self) -> str:
+        return self.title
